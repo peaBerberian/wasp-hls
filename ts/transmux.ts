a7d3baa28f134787f65addea5d29165181cd5262
Paul Berberian <pea.berberian@gmail.com>
2022-06-08 21:06:44 +0200

    Do not keep original timestamps

--- ts/transmux.ts
+++ ts/transmux.ts
@@ -77,9 +77,7 @@ export function transmux(
   inputSegment: Uint8Array
 ) : Uint8Array | null {
   if (transmuxer === undefined) {
-    transmuxer = new muxjs.mp4.Transmuxer({
-      keepOriginalTimestamps: true,
-    });
+    transmuxer = new muxjs.mp4.Transmuxer( );
   }
 
   const subSegments : Uint8Array[] = [];
