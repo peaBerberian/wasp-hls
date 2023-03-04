152bd8a22f17ee3b275f94cac809a7e28b1aa119
Paul Berberian <pea.berberian@gmail.com>
2023-03-04 23:11:12 +0100

    Export WaspHlsPlayer directly from src directory

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import WaspHlsPlayer from "./ts-main";
+
+// TODO only debug mode?
+/* eslint-disable */
+(window as any).WaspHlsPlayer = WaspHlsPlayer;
+/* eslint-enable */
+
+export default WaspHlsPlayer;
--- src/ts-main/index.ts
+++ src/ts-main/index.ts
@@ -1,8 +1,2 @@
 import WaspHlsPlayer from "./api";
-
-// TODO only debug mode?
-/* eslint-disable */
-(window as any).WaspHlsPlayer = WaspHlsPlayer;
-/* eslint-enable */
-
 export default WaspHlsPlayer;
