ab2995e2cdd3554d32f5a1765889b53ee060b0a0
Paul Berberian <pea.berberian@gmail.com>
2023-03-16 14:30:04 +0100

    Demo: add self-link for now on title

--- demo/src/components/Title.tsx
+++ demo/src/components/Title.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
 export default React.memo(function Title() : JSX.Element {
-  return <h1 className="title">{"wasp-hls player"}</h1>;
+  return <a style={{ textDecoration: "none", color: "inherit" }} href="./">
+    <h1 className="title">{"wasp-hls player"}</h1>
+  </a>;
 });
